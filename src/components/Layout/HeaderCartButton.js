83fcf850972af2bad4205578f6521eacab26bb4e
Sarvin <sarvin.to@gmail.com>
2023-04-19 00:04:13 +0200

    make the button bump when adding an item using useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,38 @@
-import React,{useContext} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+
+  const buttonClasses = `${classes.button} ${
+    !buttonIsHighlighted ? classes.bump : ""
+  }`;
 
   const cartContext = useContext(CartContext);
 
-  const totalNumberOfCartItems= cartContext.items.reduce(((currentnumber,item)=>currentnumber+item.amount),0)
+  const totalNumberOfCartItems = cartContext.items.reduce(
+    (currentnumber, item) => currentnumber + item.amount,
+    0
+  );
+
+  const items = cartContext.items;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => setButtonIsHighlighted(false), 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>{" "}
